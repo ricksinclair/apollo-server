df45c1e3f742ebbd89796634cb4be6423b9a8780
Ulric Todman <rick.sinclair.nyc@gmail.com>
2021-08-22 13:48:16 -0400

    sequelize boilerplate added basic boilerplate and imported models

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const { ApolloServer, gql } = require("apollo-server");
+const Sequelize = require("sequelize");
+const models = require("./models");
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
     hello: String
+    links: [Link]
+  }
+  type Link {
+    id: ID!
+    link: String!
+    slug: String!
+  }
+  type Mutation {
+    createLink(id: Int!, url: String!): Link
+    deleteLink(id: Int!): Boolean
   }
 `;
 
@@ -11,6 +23,11 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     hello: (root, args, context) => "Hello world!"
+  },
+
+  Link: {
+    link: () => "",
+    slug: () => ""
   }
 };
 
